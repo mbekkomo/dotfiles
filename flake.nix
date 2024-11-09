ecdecdc39d8639a83cc3b0a4e9c16aff3c9aa249
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-09 12:35:27 +0800

    changes from nixos on Sat Nov 9 12:35:27 PM WITA 2024

--- flake.nix
+++ flake.nix
@@ -12,13 +12,17 @@
     let
       system = "x86_64-linux";
       refindCommit = "5a690248b9072ab5ebb5200cfc8a76361d720a89";
+      refindTarballSha256 = ""
     in
     {
       nixosConfigurations.goat = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
           nix-flatpak.nixosModules.nix-flatpak
-          (builtins.fetch)
+          (builtins.fetchTarball {
+            url = "https://gist.github.com/betaboon/97abed457de8be43f89e7ca49d33d58d/archive/${refindCommit}.tar.gz";
+            sha256 = refindTarballSha256;
+          })
           ./configuration.nix
         ];
       };
