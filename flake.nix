6b5f586d937c9ecbe9e8d374866a8050098b9e0b
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-09 11:51:06 +0800

    changes from goats-laptop on Sat Nov 9 11:51:05 AM WITA 2024

--- flake.nix
+++ flake.nix
@@ -1,15 +1,16 @@
 {
-  description = "";
+  description = "HP 240 G5 Notebook NixOS configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    nixosConfigurations.goat = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
   };
 }
