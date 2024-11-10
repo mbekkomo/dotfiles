d76a549848a9e1732aa00065f0d6ee370d0b0347
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-10 20:14:11 +0800

    changes from nixos on Sun Nov 10 08:14:11 PM WITA 2024

--- configs/hyprland.nix
+++ configs/hyprland.nix
@@ -5,6 +5,6 @@ in
 {
   exec-once = builtins.concatStringsSep ";" [
     "systemctl --user start dunst.service"
-    "systemctl --user start"
+    "systemctl --user start plasma-polkit-agent.service"
   ];
 }
