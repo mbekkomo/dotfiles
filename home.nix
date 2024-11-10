7279bde9cea2684a5cfa3327016adac5964d385f
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-10 18:33:35 +0800

    changes from nixos on Sun Nov 10 06:33:34 PM WITA 2024

--- home.nix
+++ home.nix
@@ -57,7 +57,6 @@ in
   home.file = {
     ".config/alacritty/themes/tokyonight.toml".source = ./configs/alacritty/tokyonight.toml;
     ".config/zls.json".text = builtins.toJSON (import ./configs/zls.nix { });
-    ".local/share/fonts".source = ./fonts/DepartureMonoNerdFont-Regular.otf;
     ".config/fish/lscolors.fish".source = ./etc/lscolors.fish;
     ".config/fish/functions/nixs.fish".source = ./shells/nixs.fish;
     ".config/fish/functions/nixd.fish".source = ./shells/nixd.fish;
