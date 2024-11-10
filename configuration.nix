d8630e6f79645eb65a5273b038b8f6104902986f
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-10 09:35:59 +0800

    changes from nixos on Sun Nov 10 09:35:59 AM WITA 2024

--- configuration.nix
+++ configuration.nix
@@ -37,12 +37,6 @@
   services.xserver.enable = true;
 
   services.greetd.enable = true;
-  services.greetd.settings = {
-    default_session = {
-      command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.regreet}/bin/regreet";
-      user = "greeter";
-    };
-  };
 
   programs.regreet.enable = true;
   
