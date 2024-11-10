9b8ef78649d2267f129f1796f16a5f0da7a73b72
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-10 20:09:49 +0800

    changes from nixos on Sun Nov 10 08:09:48 PM WITA 2024

--- configs/hyprland.nix
+++ configs/hyprland.nix
@@ -3,8 +3,8 @@ let
   terminal = "alacritty";
 in
 {
-  exec-once = builtins.concatStringsSep " & " [
-    "dunst"
-
+  exec-once = builtins.concatStringsSep ";" [
+    "systemctl --user start dunst.service"
+    "systemctl --user start"
   ];
 }
