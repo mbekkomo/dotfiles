97f08895a587081f5f8e8f172e8dfbef1fa6d449
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-11-10 22:06:15 +0800

    changes from nixos on Sun Nov 10 10:06:15 PM WITA 2024

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,29 @@ in
   ];
 
   networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "none";
+  networking.nameservers = [
+    "127.0.0.1"
+    "::1"
+  ];
+
+  services.dnscrypt-proxy2.enable = true;
+  services.dnscryp-proxy2.settings = {
+    ipv6_servers = true;
+    require_dnssec = true;
+    sources.public-resolvers = {
+      urls = [
+        "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+        "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+      ];
+      cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+      minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+    };
+    server_names = [ "mullvad-base-doh" "cloudflare" ];
+  };
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
+  };
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
@@ -109,6 +132,8 @@ in
   };
 
   services.openssh.enable = true;
+
+  services.resolved.enable = false;
 
   services.flatpak.enable = true;
   services.flatpak.packages = [
