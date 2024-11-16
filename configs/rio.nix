9f487e1c06a175a38a88b3a6ef56d1227ec29b05
Komo <afiqquraisyzulkarnain@gmail.com>
2024-11-17 03:04:44 +0800

    changes from nixos on Sun Nov 17 03:04:44 AM WITA 2024

--- configs/rio.nix
+++ configs/rio.nix
@@ -4,7 +4,7 @@
 
   fonts.size = 10;
   fonts.family = "DepartureMono Nerd Font";
-  fonts.emoji = "Noto Color Emoji";
+  fonts.emoji.family = "Noto Color Emoji";
 
   renderer.performance = "High";
   renderer.backend = "Vulkan";
