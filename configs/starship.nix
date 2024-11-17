df24f4d5d39a778729a9f1e55f955a89563f1c50
Komo <afiqquraisyzulkarnain@gmail.com>
2024-11-17 15:24:11 +0800

    changes from nixos on Sun Nov 17 03:24:11 PM WITA 2024

--- configs/starship.nix
+++ configs/starship.nix
@@ -106,5 +106,5 @@ in
       }
       [ └─╴](grey)$character'';
 
-  palettes.catppuccin_mocha = config.palettes.catppuccin_mocha // (lib.mkDefault palette);
+  palettes.catppuccin_mocha = palette // config.palettes.catppuccin_mocha;
 }
