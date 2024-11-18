b5f9bf0e4ede94243183a9b8a00957af3b428af9
Komo <afiqquraisyzulkarnain@gmail.com>
2024-11-18 20:00:06 +0800

    changes from nixos on Mon Nov 18 08:00:06 PM WITA 2024

---
+++ configs/wluma.nix
@@ -0,0 +1,10 @@
+{ ... }: {
+  als.none = {};
+  output.backlight = [
+    {
+      name = "eDP-1";
+      path = "/sys/class/backlight/intel_backlight";
+      capturer = "wayland";
+    }
+  ];
+}
