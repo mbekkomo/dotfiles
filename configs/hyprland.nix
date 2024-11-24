5062b6e944ce937824888b94dc06ee6c065b3c80
Komo <afiqquraisyzulkarnain@gmail.com>
2024-11-24 17:21:41 +0800

    changes from nixos on Sun Nov 24 05:21:41 PM WITA 2024

--- configs/hyprland.nix
+++ configs/hyprland.nix
@@ -6,7 +6,7 @@ let
   cursor_size = 30;
 in
 {
-  exec-once = builtins.concatStringsSep ";" [
+  exec-once = [
     "systemctl --user start dunst.service"
     "systemctl --user start hyprpolkitagent.service"
     "systemctl --user start hypridle.service"
@@ -19,6 +19,20 @@ in
     "XCURSOR_SIZE,${toString cursor_size}"
     "HYPRCURSOR_SIZE,${toString cursor_size}"
   ];
+
+  general = {
+    gaps_in = 5;
+    gaps_out = 20;
+
+    "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+    "col.inactive_border" = "rgba(595959aa)";
+
+    resize_on_border = true;
+
+    allow_tearing = false;
+
+    layout = "dwindle";
+  };
 
   bindel = [
     ",XF86MonBrightnessUp, exec, ${pkgs.brightnessctl}/bin/brightnessctl s 10%+"
